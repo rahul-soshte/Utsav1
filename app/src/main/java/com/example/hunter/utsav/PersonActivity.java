6f1cc9ee027089a44050eb3557978f4f95df2604
rahul-soshte <rahul.soshte47@gmail.com>
2017-02-08 03:06:26 +0530

    Data Placing from database done

--- app/src/main/java/com/example/hunter/utsav/PersonActivity.java
+++ app/src/main/java/com/example/hunter/utsav/PersonActivity.java
@@ -1,10 +1,14 @@
 package com.example.hunter.utsav;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import static com.example.hunter.utsav.UtsavDatabaseHelper.tableName;
 
 public class PersonActivity extends Activity implements View.OnClickListener {
 
@@ -15,7 +19,10 @@ public class PersonActivity extends Activity implements View.OnClickListener {
     private Button btnMessage;
     private Button btnDelete;
     private Button btnUpdate;
+    SQLiteDatabase newDB;
+
     public static final String EXTRA_PERSON = "Person_No";
+private Cursor c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +39,39 @@ public class PersonActivity extends Activity implements View.OnClickListener {
         btnUpdate.setOnClickListener(this);
         btnMessage.setOnClickListener(this);
         btnDelete.setOnClickListener(this);
+        int Person_No = (Integer) getIntent().getExtras().get(EXTRA_PERSON);
+        UtsavDatabaseHelper dbHelper = new UtsavDatabaseHelper(this.getApplicationContext());
+        newDB = dbHelper.getWritableDatabase();
+         c = newDB.rawQuery("SELECT FirstName, Class, Phone FROM " +
+                tableName , null);
+        showRecord(Person_No);
+
+
 
     }
+    public void showRecord(int rec)
+    {
+        int i=0;
+
+if(c!=null) {
+    c.moveToFirst();
+    while(i!=rec)
+    {
+        c.moveToNext();
+        i++;
+
+    }
+}
+        
+        String name=c.getString(c.getColumnIndex("FirstName"));
+        String age=c.getString(c.getColumnIndex("Class"));
+        String Phone=c.getString(c.getColumnIndex("Phone"));
+
+        editTextName.setText(name);
+        editTextClass.setText(age);
+        editTextPhoneNo.setText(Phone);
+    }
+
 
     public void onClick(View v) {
         if (v == btnUpdate) {
